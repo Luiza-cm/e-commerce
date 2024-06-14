d752ce19dba62658217005a2a086a3be14b2b547
Luiza Moncao <luiza.cmoncao@gmail.com>
2024-06-14 15:26:44 -0300

    Finishing home page product card in a grid layout

--- lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_learnings/common/styles/shadows.dart';
 import 'package:flutter_learnings/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:flutter_learnings/common/widgets/icons/circular_icon.dart';
 import 'package:flutter_learnings/common/widgets/images/rounded_image.dart';
+import 'package:flutter_learnings/common/widgets/texts/product_price_text.dart';
+import 'package:flutter_learnings/common/widgets/texts/product_title_text.dart';
 import 'package:flutter_learnings/utils/constants/colors.dart';
 import 'package:flutter_learnings/utils/constants/image_strings.dart';
 import 'package:flutter_learnings/utils/constants/sizes.dart';
@@ -17,58 +17,116 @@ class ProductCardVertical extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = AppHelperFunctions.isDarkMode(context);
-    return Container(
-      width: 180,
-      padding: const EdgeInsets.all(1),
-      decoration: BoxDecoration(
-          boxShadow: [ShadowStyle.verticalProductShadow],
-          borderRadius: BorderRadius.circular(AppSizes.productImageRadius),
-          color: dark ? MyColors.darkerGrey : MyColors.white),
-      child: Column(
-        children: [
-          /// Thumbnail, Wishlist Button, Discount Tag
-          RoundedContainer(
-            height: 180,
-            padding: const EdgeInsets.all(AppSizes.sm),
-            backgroundColor: dark ? MyColors.dark : MyColors.light,
-            child: Stack(
-              children: [
-                /// Thumbnail image
-                const RoundedImage(
-                    imageUrl: AppImages.productImage1, applyImageRadius: true),
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        width: 180,
+        padding: const EdgeInsets.all(1),
+        decoration: BoxDecoration(
+            boxShadow: [ShadowStyle.verticalProductShadow],
+            borderRadius: BorderRadius.circular(AppSizes.productImageRadius),
+            color: dark ? MyColors.darkerGrey : MyColors.white),
+        child: Column(
+          children: [
+            /// Thumbnail, Wishlist Button, Discount Tag
+            RoundedContainer(
+              height: 180,
+              padding: const EdgeInsets.all(AppSizes.sm),
+              backgroundColor: dark ? MyColors.dark : MyColors.light,
+              child: Stack(
+                children: [
+                  /// Thumbnail image
+                  const RoundedImage(
+                      imageUrl: AppImages.productImage1,
+                      applyImageRadius: true),
 
-                /// Sale tag
-                Positioned(
-                  top: 12,
-                  child: RoundedContainer(
-                    radius: AppSizes.sm,
-                    backgroundColor: MyColors.secondary.withOpacity(0.8),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: AppSizes.sm, vertical: AppSizes.xs),
-                    child: Text(
-                      '25%',
-                      style: Theme.of(context)
-                          .textTheme
-                          .labelLarge!
-                          .apply(color: MyColors.black),
+                  /// Sale tag
+                  Positioned(
+                    top: 12,
+                    child: RoundedContainer(
+                      radius: AppSizes.sm,
+                      backgroundColor: MyColors.secondary.withOpacity(0.8),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: AppSizes.sm, vertical: AppSizes.xs),
+                      child: Text(
+                        '25%',
+                        style: Theme.of(context)
+                            .textTheme
+                            .labelLarge!
+                            .apply(color: MyColors.black),
+                      ),
                     ),
                   ),
-                ),
 
-                /// Favourite icon button
-                const Positioned(
-                    top: 0,
-                    right: 0,
-                    child: CircularIcon(
-                      icon: Iconsax.heart5,
-                      color: Colors.red,
-                    ))
-              ],
+                  /// Favourite icon button
+                  const Positioned(
+                      top: 0,
+                      right: 0,
+                      child: CircularIcon(
+                        icon: Iconsax.heart5,
+                        color: Colors.red,
+                      )),
+                ],
+              ),
             ),
-          )
 
-          /// Details
-        ],
+            const SizedBox(height: AppSizes.spaceBtwItems / 2),
+
+            /// Details
+            Padding(
+                padding: const EdgeInsets.only(left: AppSizes.sm),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const ProductTitleText(
+                      title: 'Bota Coturno Cano Baixo',
+                      textAlign: TextAlign.left,
+                      smallSize: true,
+                    ),
+                    const SizedBox(height: AppSizes.spaceBtwItems / 2),
+                    Row(
+                      children: [
+                        Text(
+                          'Schultz',
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                        const SizedBox(width: AppSizes.xs),
+                        const Icon(
+                          Iconsax.verify5,
+                          color: MyColors.primary,
+                          size: AppSizes.iconXs,
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const ProductPriceText(
+                          price: '89,90',
+                        ),
+                        Container(
+                          decoration: const BoxDecoration(
+                            color: MyColors.dark,
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(AppSizes.cardRadiusMd),
+                                bottomLeft: Radius.circular(
+                                    AppSizes.productImageRadius)),
+                          ),
+                          child: const SizedBox(
+                              width: AppSizes.iconLg * 1.2,
+                              height: AppSizes.iconLg * 1.2,
+                              child: Center(
+                                  child: Icon(Iconsax.add,
+                                      color: MyColors.white))),
+                        )
+                      ],
+                    )
+                  ],
+                ))
+          ],
+        ),
       ),
     );
   }
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,17 +1,12 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_learnings/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:flutter_learnings/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:flutter_learnings/common/widgets/custom_shapes/containers/search_container.dart';
-import 'package:flutter_learnings/common/widgets/image_text_widgets/vertical_image_text.dart';
-import 'package:flutter_learnings/common/widgets/images/rounded_image.dart';
+import 'package:flutter_learnings/common/widgets/layouts/grid.layout.dart';
 import 'package:flutter_learnings/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:flutter_learnings/common/widgets/texts/section_heading.dart';
 import 'package:flutter_learnings/utils/constants/colors.dart';
 import 'package:flutter_learnings/utils/constants/image_strings.dart';
 import 'package:flutter_learnings/utils/constants/sizes.dart';
-import 'package:flutter_learnings/utils/helpers/helper_functions.dart';
 import 'widgets/home_appbar.dart';
 import 'widgets/home_categories.dart';
 import 'widgets/promo_slider.dart';
@@ -21,11 +16,11 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
           child: Column(
         children: [
-          AppPrimaryHeaderContainer(
+          const AppPrimaryHeaderContainer(
             child: Column(
               children: [
                 /// Appbar
@@ -59,18 +54,21 @@ class HomeScreen extends StatelessWidget {
 
           /// Body - White container
           Padding(
-            padding: EdgeInsets.all(AppSizes.defaultSpace),
+            padding: const EdgeInsets.all(AppSizes.defaultSpace),
             child: Column(
               children: [
-                PromoSlider(
+                const PromoSlider(
                   banners: [
                     AppImages.promoBanner1,
                     AppImages.promoBanner2,
                     AppImages.promoBanner3
                   ],
                 ),
-                SizedBox(height: AppSizes.spaceBtwSections),
-                ProductCardVertical(),
+                const SizedBox(height: AppSizes.spaceBtwSections),
+                GridLayout(
+                  itemCount: 4,
+                  itemBuilder: (_, index) => const ProductCardVertical(),
+                ),
               ],
             ),
           )
